5bc931847e634e173336ae36a052b99ca36d0ba5
Jersy2109 <jersy2109@gmail.com>
2022-11-29 00:23:27 -0600

    Changed replay size for opt

--- opt.py
+++ opt.py
@@ -489,4 +489,4 @@ def training(env_name, replay_memory_size=1_000_000, max_frames=50_000_000, gamm
 
 if __name__ == '__main__':
     import sys
-    training(env_name=sys.argv[1], verbose=False)
+    training(env_name=sys.argv[1], verbose=False, replay_memory_size=500_000)
