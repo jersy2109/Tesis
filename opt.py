4623f7391d52436853fd9cd86aad0e25aaceba63
jersy2109 <jersy2109@gmail.com>
2022-11-29 02:37:15 -0600

    Changed buffer sample implementation

--- opt.py
+++ opt.py
@@ -355,7 +355,10 @@ class ExperienceReplay:
         self.buffer.append(Experience(*args))
 
     def sample(self, batch_size):
-        return random.sample(self.buffer, batch_size)
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
+               np.array(dones, dtype=np.uint8), np.array(next_states)
 
 
 ### Agent
