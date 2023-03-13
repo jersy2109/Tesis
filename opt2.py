f0be5f8d30ab792533ac3b0a41d87faf37bfa233
Jersy2109 <jersy2109@gmail.com>
2023-03-13 15:44:34 -0600

    Only opt

--- opt2.py
+++ opt2.py
@@ -577,8 +577,8 @@ def training(env_name, replay_memory_size=50_000, max_frames=5_000_000, gamma=0.
 
 if __name__ == '__main__':
     import sys
-    for game in ["SpaceInvaders", "Pong", "MsPacman"]:
+    for game in ["SpaceInvaders", "Pong", "MsPacman", "Breakout", "Atlantis"]:
         for size in [50_000, 70_000, 100_000]:
             training(env_name=game, replay_memory_size=size, verbose=False, opt=True)
-            training(env_name=game, replay_memory_size=size, verbose=False, opt=False)
+            #training(env_name=game, replay_memory_size=size, verbose=False, opt=False)
         
