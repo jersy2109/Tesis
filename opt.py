e6001531b7e65a0ba64d882ffd2aebbdb2b0c310
jersy2109 <jersy2109@gmail.com>
2023-03-31 18:13:31 -0600

    Now training Breakout and Atlantis

--- opt.py
+++ opt.py
@@ -529,8 +529,7 @@ def training(env_name, replay_memory_size=150_000, max_frames=10_000_000, gamma=
 
 if __name__ == '__main__':
     #import sys
-    # "Breakout", "SpaceInvaders", "Atlantis"
-    # MSPACMAN FALTAN 100 Y 150K
-    for game in ["Pong", "MsPacman"]:
+    # "SpaceInvaders", "Enduro"
+    for game in ["Breakout", "Atlantis"]:
         for size in [50_000, 75_000, 100_000, 150_000]:
             training(env_name=game, replay_memory_size=size, verbose=False)
