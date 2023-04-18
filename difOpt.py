947e83f473405cd373e4d2e69bad2e2b78f1a080
Jersy2109 <jersy2109@gmail.com>
2023-04-18 17:43:24 -0600

    Dif opt added

--- difOpt.py
+++ difOpt.py
@@ -1,21 +1,20 @@
-# Imports
-
+import os
 import gym
 import gym.spaces
 import numpy as np
 import cv2 as cv
-from collections import deque
+import matplotlib.pyplot as plt
+from collections import deque, namedtuple
 from tqdm import tqdm
+import datetime
 import random
 import pickle
-import os
+from pathlib import Path
 from natsort import natsorted
-import datetime
 
 import torch
 import torch.nn as nn
-
-# Helpers 
+import torch.optim as optim
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -25,7 +24,7 @@ def set_seed(seed, env):
     random.seed(seed)
     np.random.seed(seed)
 
-# Wrappers
+### Wrappers
 
 class NoopResetEnv(gym.Wrapper):
     def __init__(self, env, noop_max=30):
@@ -44,21 +43,21 @@ class NoopResetEnv(gym.Wrapper):
             if done:
                 obs = self.env.reset()
         return obs
-    
+
 
 class MaxAndSkipEnv(gym.Wrapper):
     def __init__(self, env, skip=4):
         gym.Wrapper.__init__(self, env)
         self._skip = skip
-        self._obs_buffer = np.zeros((2,) + self.env.observation_space.shape, dtype=np.uint8)
+        self._obs_buffer = np.zeros((self._skip,) + self.env.observation_space.shape, dtype=np.uint8)
 
     def step(self, action):
         total_reward = 0.0
         done = None
         for i in range(self._skip):
             obs, reward, done, _, info = self.env.step(action)
-            if i == 0: self._obs_buffer[0] = obs
-            if i == self._skip - 1: self._obs_buffer[1] = obs            
+            if i == self._skip - 2: self._obs_buffer[0] = obs
+            if i == self._skip - 1: self._obs_buffer[1] = obs           
             total_reward += reward
             if done:
                 break
@@ -66,9 +65,8 @@ class MaxAndSkipEnv(gym.Wrapper):
         return max_frame, total_reward, done, info
 
     def reset(self):
-        obs = self.env.reset()
-        return obs
-    
+        return self.env.reset()
+        
 
 class TimeLimit(gym.Wrapper):
     def __init__(self, env, max_episode_steps=None):
@@ -86,9 +84,8 @@ class TimeLimit(gym.Wrapper):
 
     def reset(self):
         self._elapsed_steps = 0
-        obs = self.env.reset()
-        return obs
-    
+        return self.env.reset()
+        
 
 class FireResetEnv(gym.Wrapper):
     def __init__(self, env):
@@ -111,9 +108,8 @@ class FireResetEnv(gym.Wrapper):
         if self.lives > self.env.ale.lives():
             self.lives = self.env.ale.lives()
             action = 1
-        obs, reward, done, info = self.env.step(action)
-        return obs, reward, done, info
-    
+        return self.env.step(action)
+        
 
 class ClipReward(gym.RewardWrapper):
     def __init__(self, env, min_r=-1, max_r=1):
@@ -132,7 +128,7 @@ class ClipReward(gym.RewardWrapper):
     def step(self, action):
         obs, rew, done, info = self.env.step(action)
         return obs, self.reward(rew), done, info
-    
+
 
 class WarpFrame(gym.ObservationWrapper):
     def __init__(self, env, width=84, height=84):
@@ -163,7 +159,7 @@ class WarpFrame(gym.ObservationWrapper):
     def reset(self):
         obs = self.env.reset()
         return self.observation(obs)
-    
+
 
 class FrameStack(gym.Wrapper):
     def __init__(self, env, k=2):
@@ -191,7 +187,7 @@ class FrameStack(gym.Wrapper):
     def _get_obs(self):
         assert len(self.frames) == self.k
         return self.frames
-    
+
 
 class OpticalFlowCV(gym.ObservationWrapper):
     def __init__(self, env):
@@ -205,29 +201,11 @@ class OpticalFlowCV(gym.ObservationWrapper):
 
     def observation(self, obs):
         assert np.array(obs).shape == (2, 84, 84, 3)
-        frames = obs
 
-        first_frame = np.array(frames)[0].astype('uint8')
-        prev_gray = cv.cvtColor(first_frame, cv.COLOR_RGB2GRAY)
-
-        mask = np.zeros_like(first_frame)
-        mask[..., 1] = 255
-
-        frame = np.array(frames)[1].astype('uint8')
-        gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-
-        flow = cv.calcOpticalFlowFarneback(prev_gray, gray,
-                                        None, 
-                                        0.5, 5, 5, 5, 7, 1.5, 0)
-
-        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
-
-        mask[..., 0] = angle * 180 / np.pi / 2
-        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
-
-        flow = cv.cvtColor(mask, cv.COLOR_HSV2RGB)
+        flow = np.array(obs[1]-obs[0])
         obs[0] = flow
-        obs[1] = np.array(frames)[1]
+        #obs[1] = np.array(frames)[1]
+        
         assert np.array(obs).shape == (2, 84, 84, 3)
         return obs
 
@@ -238,7 +216,6 @@ class OpticalFlowCV(gym.ObservationWrapper):
     def reset(self):
         return self.env.reset()
     
-
 class ScaledFloatFrame(gym.ObservationWrapper):
     def __init__(self, env):
         gym.ObservationWrapper.__init__(self, env)
@@ -263,7 +240,6 @@ class ScaledFloatFrame(gym.ObservationWrapper):
     def reset(self):
         obs = self.env.reset()
         return self.observation(obs)
-    
 
 class EpisodicLifeEnv(gym.Wrapper):
     """
@@ -292,7 +268,8 @@ class EpisodicLifeEnv(gym.Wrapper):
         self.lives = self.env.unwrapped.ale.lives()
         return obs
     
-# Ambiente
+
+### Environment
 
 def make_atari(env_id, max_episode_steps=1_000, noop_max=30, skip=4, sample=False):
     env = gym.make(env_id, render_mode=None)
@@ -312,7 +289,8 @@ def make_atari(env_id, max_episode_steps=1_000, noop_max=30, skip=4, sample=Fals
     env = ScaledFloatFrame(env)
     return env
 
-# RED
+
+### Network 
 
 class DQN(nn.Module):
     def __init__(self, input_shape, n_actions):
@@ -329,7 +307,7 @@ class DQN(nn.Module):
 
         conv_out_size = self._get_conv_out(input_shape)
         self.fc = nn.Sequential(
-            nn.Linear(conv_out_size, 512),
+            nn.Linear(conv_out_size, 512), ##############
             nn.ReLU(),
             nn.Linear(512, n_actions)
         )
@@ -341,7 +319,192 @@ class DQN(nn.Module):
     def forward(self, x):
         conv_out = self.conv(x).view(x.size()[0], -1)
         return self.fc(conv_out)
+
+
+### Experience Replay
+
+Experience = namedtuple('Experience', field_names=['state','action','reward','done','next_state'])
+
+class ExperienceReplay:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, *args):
+        self.buffer.append(Experience(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
+
+
+### Agent
+
+class Agent:
+    def __init__(self, env, exp_buffer):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self._reset()
+
+    def _reset(self):
+        self.state = self.env.reset()
+        self.total_reward = 0.0
+
+    def play_step(self, net, epsilon=0.0, device='cuda'):
+        done_reward = None
+
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            state_a = np.array([self.state], copy=False)
+            state_v = torch.tensor(state_a).to(device)
+            q_vals_v = net(state_v)
+            _, act_v = torch.max(q_vals_v, dim=1)
+            action = int(act_v.item())
+
+        new_state, reward, done, _ = self.env.step(action)
+        self.total_reward += reward
+        
+        self.exp_buffer.append(self.state, action, reward, done, new_state)
+        self.state = new_state
+
+        if done:
+            done_reward = self.total_reward
+            self._reset()
+
+        return done_reward
+
+
+### Training
+
+def episode_stopping(timer):
+    delta = datetime.timedelta(seconds=10)
+    if datetime.datetime.now()-timer > delta:
+        return True
+
+def training(env_name, replay_memory_size=150_000, max_frames=10_000_000, gamma=0.99, batch_size=32,  \
+            learning_rate=0.00025, sync_target_frames=10_000, net_update=4, replay_start_size=50_000, \
+            eps_start=1, eps_min=0.1, seed=2109, device='cuda', verbose=True):
+    """
+    Función de entrenamiento.
+    """
+    numberOfDicts = 25
+
+    filename = env_name + "_DifOpt_" +  str(int(replay_memory_size/1_000)) + "k"
+    path = "dicts/" + filename
+    Path(path).mkdir(parents=True, exist_ok=True)
     
+    env = make_atari(env_name + "NoFrameskip-v4")
+    buffer = ExperienceReplay(replay_memory_size)
+    agent = Agent(env, buffer)
+    set_seed(seed=seed, env=env)
+    
+    net        = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    target_net = DQN(env.observation_space.shape, env.action_space.n).to(device)
+    
+    epsilon = eps_start
+    eps_decay = (eps_start - eps_min) / replay_memory_size
+    
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    total_rewards = []
+    loss_history = []
+    tr_finished = True
+
+    best_mean_reward = None
+    start_time = datetime.datetime.now()
+
+    for frame in tqdm(range(1, max_frames+1), desc=filename):        
+        reward = agent.play_step(net, epsilon, device)
+
+        if reward is not None:
+            total_rewards.append(reward)
+            mean_reward = np.mean(total_rewards[-100:])
+            
+            time_passed = datetime.datetime.now() - start_time
+
+            if best_mean_reward is None or best_mean_reward < mean_reward:
+                torch.save(net.state_dict(), path + "/" + filename + "_DifBest.dat")
+                best_mean_reward = mean_reward
+
+        if len(buffer) < replay_start_size:
+            continue
+
+        start_frame = datetime.datetime.now()
+        epsilon = max(epsilon-eps_decay, eps_min)
+
+        if frame % net_update == 0:
+            sardn = buffer.sample(batch_size)
+            batch = Experience(*zip(*sardn))
+            
+            states_v = torch.tensor(np.array(batch.state)).to(device)
+            next_states_v = torch.tensor(np.array(batch.next_state)).to(device)
+            actions_v = torch.tensor(batch.action).to(device)
+            rewards_v = torch.tensor(batch.reward).to(device)
+            done_mask = torch.BoolTensor(batch.done).to(device)
+
+            state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+            next_state_values = target_net(next_states_v).max(1)[0]
+            next_state_values[done_mask] = 0.0
+            next_state_values = next_state_values.detach()
+            expected_state_action_values = next_state_values*gamma + rewards_v
+            
+            loss_t = nn.HuberLoss()(state_action_values, expected_state_action_values) # MSELoss()(input,target)
+
+            loss_history.append(loss_t.item())
+            optimizer.zero_grad()
+            loss_t.backward()
+            optimizer.step()
+
+        if frame % sync_target_frames == 0:
+            target_net.load_state_dict(net.state_dict())
+
+        if frame % (max_frames // numberOfDicts) == 0:
+            if verbose:
+                print("{}:  {} games, best result {:.3f}, mean reward {:.3f}, eps {:.2f}, time {}".format(
+                    frame, len(total_rewards), max(total_rewards), mean_reward, epsilon, time_passed))
+            torch.save(net.state_dict(), path + "/" + filename + '_' + str(int((frame)/(max_frames//numberOfDicts))) + "k.dat")
+
+        if episode_stopping(start_frame):
+            print("Taking too long")
+            tr_finished = False
+            break 
+
+    end_time = datetime.datetime.now() - start_time
+    print("Training finished")
+    print("{}:  {} games, mean reward {:.3f}, eps {:.2f}, time {}".format(
+            frame, len(total_rewards), mean_reward, epsilon, end_time))
+        
+    pkl_file = "dicts/" + filename + "/" + filename + "_DifTotal.pkl"
+    with open(pkl_file, 'wb+') as f:
+        pickle.dump(total_rewards, f)
+    pkl_file = "dicts/" + filename + "/" + filename + "_DifLoss.pkl"
+    with open(pkl_file, 'wb+') as f:
+        pickle.dump(loss_history, f)
+
+    parameters = "Environment: {} \
+                \nOptical: True \
+                \nReplay Memory Size: {} \
+                \nMax Frames: {} \
+                \nGamma: {} \
+                \nBatch Size: {} \
+                \nLearning Rate: {} \
+                \nSync Target Frames: {} \
+                \nNet Update: {} \
+                \nReplay Start Size: {} \
+                \nInitial Epsilon: {} \
+                \nMinimum Epsilon: {} \
+                \nRandom Seed: {} \
+                \nFinished Training: {} \
+                \nTraining Time: {}".format(env_name,replay_memory_size,max_frames,gamma,batch_size,learning_rate,
+                                        sync_target_frames,net_update,replay_start_size,eps_start,eps_min,seed,tr_finished,end_time)
+    
+    aux_file = "dicts/" + filename + "/" + filename + "_DifParameters.txt"
+    with open(aux_file, 'w+') as f:
+        f.write(parameters)
+
+    return total_rewards, loss_history
+
 # Evaluación
 
 def sample(game, model, model_name, n_samples=30, verbose=True):
@@ -350,7 +513,7 @@ def sample(game, model, model_name, n_samples=30, verbose=True):
     '''
     game = game + 'NoFrameskip-v4'
     model = 'dicts/' + model + '/' + model_name
-    env = make_atari(game, sample=True, skip=6)
+    env = make_atari(game, sample=True, max_episode_steps=5_000, skip=6)
     net = DQN(env.observation_space.shape, env.action_space.n)
     net.load_state_dict(torch.load(model, map_location=lambda storage, loc: storage))
     epsilon = 0.05 
@@ -384,12 +547,10 @@ def sample(game, model, model_name, n_samples=30, verbose=True):
 
     return rewards
 
-
 def get_dats_files(path):
     dats = [x for x in os.listdir(path) if 'dat' in x]
     return natsorted(dats)
-
-
+ 
 def sample_model(game, samples=30, directory=None):
     if directory:
         dats_array = [natsorted([x for x in os.listdir(directory) if 'dat' in x])]
@@ -404,7 +565,7 @@ def sample_model(game, samples=30, directory=None):
             model_rewards.append(rw)
         game_rewards.append(model_rewards)
 
-    pkl_file = "samples/" + game + "_Sample_rewards.pkl"
+    pkl_file = "samples/" + game + "_DifSample_rewards.pkl"
     with open(pkl_file, 'wb+') as f:
         pickle.dump(game_rewards, f)
     return np.array(game_rewards, dtype=object)
@@ -412,4 +573,9 @@ def sample_model(game, samples=30, directory=None):
 
 if __name__ == '__main__':
     import sys
-    sample_model(game=sys.argv[1], directory=sys.argv[2])
+    GAME = sys.argv[1]
+    SIZE = int(sys.argv[2])
+    FRAMES = int(sys.argv[3])
+    path = "dicts/" + GAME + "_DifOpt_" +  str(int(SIZE/1_000)) + "k"
+    training(env_name=GAME, replay_memory_size=SIZE, verbose=False, max_frames=FRAMES)
+    sample_model(game=GAME, directory=path, samples=30)
