8775ab263bbc88de4a363c064ae61db37548dcbb
Jersy2109 <jersy2109@gmail.com>
2023-07-04 13:09:26 -0600

    stupid ass

--- opt.py
+++ opt.py
@@ -550,7 +550,7 @@ def sample(game, model, model_name, n_samples=30, verbose=True):
 
     rewards = np.zeros(n_samples)
 
-    for i in range(tqdm(n_samples)):
+    for i in tqdm(range(n_samples)):
         game_timer = datetime.datetime.now()
         state = env.reset()
         total_reward = 0.0
